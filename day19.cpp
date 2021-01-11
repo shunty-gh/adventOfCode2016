82ac4f104f106e5c7d67151e9385740ff9db566f
Steven Hunt <sph@shunty.co.uk>
2021-01-11 12:24:24 +0000

    Add commencts to day 19

--- day19.cpp
+++ day19.cpp
@@ -22,15 +22,25 @@ int main() {
     int part1 = ((Input - (1<<21)) * 2) + 1;
     std::cout << "Part 1: " << part1 << std::endl;
 
+    /* Later...
+       It turns out that this is the [Josephus Problem](https://en.wikipedia.org/wiki/Josephus_problem)
+       and the solution given on the wikipedia page is:
+         2(n - 2^Log2(n)) + 1
+
+       Who knew.
+    */
+
     /* For part 2 we'll build a single linked list and maintain a
        pointer to the element *before* the next item to be deleted.
        Call this the delete pointer (dp). This means that dp->Next is
-       the next one to be deleted and therefore dp->Next->Next will
+       the next item to be deleted and therefore dp->Next->Next will
        be the new value that dp will point to after removal.
        Due to the fact that we will always be deleting at a point n/2
        elements away from the current, and the current always advances
        by 1 we see that the delete pointer advances only when n changes
-       from odd to even
+       from odd to even. (Due to the AoC rule: "If two Elves are across
+       the circle, the one on the left (from the perspective of the
+       stealer) is stolen from.")
     */
     Node first{1};
     Node* n1 = &first;
